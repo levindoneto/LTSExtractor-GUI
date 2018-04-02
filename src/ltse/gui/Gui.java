a6c2abdace19d6003360d7f6c779d5f89a073c89
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-04-02 00:16:41 -0300

    Add icon and fixed size configuration

--- src/ltse/gui/Gui.java
+++ src/ltse/gui/Gui.java
@@ -1,10 +1,16 @@
 import javax.swing.JFrame;
+import javax.swing.ImageIcon;
 public class Gui {
+    int height = 800;
+    int width = 640;
     public Gui() {
         JFrame guiLtse = new JFrame();
+        ImageIcon img = new ImageIcon("../../../Resources/icon.png");
+        guiLtse.setIconImage(img.getImage());
         guiLtse.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        guiLtse.setTitle("LTSE");
-        guiLtse.setSize(640,800);
+        guiLtse.setTitle("LTS Extractor");
+        guiLtse.setSize(width,height);
+        guiLtse.setResizable(false); // fixed size
         guiLtse.setVisible(true);
     }
     public static void main(String[] args) {
