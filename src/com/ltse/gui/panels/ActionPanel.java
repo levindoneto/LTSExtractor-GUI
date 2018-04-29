dc012d9ce02253c90601533153d4abd210dfdefd
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-04-29 15:32:45 -0300

    Add overridden methods for obtaining panel components

--- src/com/ltse/gui/panels/ActionPanel.java
+++ src/com/ltse/gui/panels/ActionPanel.java
@@ -1,8 +1,18 @@
 package com.ltse.gui.panels;
 
+import javax.swing.*;
+
 /* Panel with a label "Action" and with 3 checkboxes:
  * Call Mode, Termination Mode and Enter/Exit Mode
  */
 public class ActionPanel extends Panel {
 	public ActionPanel() {}
+
+	@Override
+	public JPanel getPanel(int x, int y, JButton buttonPanel) {
+		System.out.println("ActionPanel");
+		JPanel contentPanel = new JPanel();
+
+		return contentPanel;
+	}
 }
--- src/com/ltse/gui/panels/InfoPanel.java
+++ src/com/ltse/gui/panels/InfoPanel.java
@@ -1,8 +1,18 @@
 package com.ltse.gui.panels;
 
+import javax.swing.*;
+
 /* Panel with a label "Panel" and with a text box
  * which communicates with the LTSE backend
  */
 public class InfoPanel extends Panel {
 	public InfoPanel() {}
+
+	@Override
+	public JPanel getPanel(int x, int y, JButton buttonPanel) {
+		System.out.println("InfoPanel");
+		JPanel contentPanel = new JPanel();
+
+		return contentPanel;
+	}
 }
