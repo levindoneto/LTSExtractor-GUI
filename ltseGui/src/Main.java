3a730382c85a16dcbe445a397a311ff0680e1cef
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-06-03 18:12:38 -0300

    Add new swing aside project for the GUI

---
+++ ltseGui/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author lgtneto
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainPanel a = new MainPanel();
+        a.setVisible(true);
+    }
+    
+}
