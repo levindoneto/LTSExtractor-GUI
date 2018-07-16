79283d25db2d3d2a983c4c2eb4362d35e588de0c
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-07-16 00:54:24 -0300

    Add ltse icon to the window

--- ltseGui/src/Main.java
+++ ltseGui/src/Main.java
@@ -1,3 +1,10 @@
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author lgtneto
@@ -6,12 +13,16 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        MainPanel a = new MainPanel();
-        a.setTitle("LTS Extractor");
-        a.setResizable(false); // fixed size
-        a.setVisible(true);
+    public static void main(String[] args) throws IOException {
+        MainPanel ltseGui = new MainPanel();
+        URL resource = ltseGui.getClass().getResource("icon.png");
+        BufferedImage image = ImageIO.read(resource);
+        ltseGui.setIconImage(image);       
+        ltseGui.setTitle("LTS Extractor");
+        ltseGui.setResizable(false); // fixed size
+        ltseGui.setVisible(true);
     }
     
 }
